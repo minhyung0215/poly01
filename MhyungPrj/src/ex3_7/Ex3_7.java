f1084370c90c8ace4f0fe93159b521daa9059767
data30 <data30@DESKTOP-3A59H6F>
2021-03-17 11:44:13 +0900

    예제 3-7, 예제 3-9 (p.100, p.102)

---
+++ MhyungPrj/src/ex3_7/Ex3_7.java
@@ -0,0 +1,20 @@
+package ex3_7;
+
+public class Ex3_7 {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a = (int) 123.45f;
+		
+		b = 200;
+		
+		System.out.printf("a의 값 ==> %d \n", a);
+		System.out.printf("b의 값 ==> %f \n", b);
+		
+		System.out.println("a의 값 : " + a);
+		System.out.println("b의 값 : " + b);
+	}
+
+}
