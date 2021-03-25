285a9110d3ab243b4179838d56817c2f26563226
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-03-25 13:48:09 +0900

    2021.03.25
    논리연산자 or

---
+++ javatest1/Code3.java
@@ -0,0 +1,20 @@
+package javatest1;
+
+public abstract class Code3 {
+
+	public static void main(String[] args) {
+		if (true || true) {
+			System.out.println(1);
+		}
+		if (true || false) {
+			System.out.println(2);
+		}
+		if (false || true) {
+			System.out.println(3);
+		}
+		if (false || false) {
+			System.out.println(4);
+		}
+	}
+
+}
