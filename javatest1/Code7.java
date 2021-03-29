93f4ffa34352b1d577a125e1f7d4e957cf07ea32
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-03-30 08:25:15 +0900

    java
    break, continue

---
+++ javatest1/Code7.java
@@ -0,0 +1,22 @@
+package javatest1;
+
+public class Code7 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println("Coding Everybody" + i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 1; i < 10; i++) {
+			if (i == 5)
+				continue;
+			System.out.println("Coding Everybody" + i);
+			
+		}
+	}
+
+}
