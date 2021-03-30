fee10195533eb36640f186cd5e4bf7699401816a
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-03-31 08:27:28 +0900

    java
    메소드

---
+++ javatest1/Code9.java
@@ -0,0 +1,17 @@
+package javatest1;
+
+public class Code9 {
+
+	public static void numbering() {
+		int i = 0;
+		while (i <10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+	
+	public static void main(String[] args){
+		numbering();
+	}
+
+}
