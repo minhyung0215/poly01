290151c1afc975963db8f26091d2f5020dce346b
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-04-02 08:36:49 +0900

    java
    유효범위

---
+++ javatest1/Code11.java
@@ -0,0 +1,18 @@
+package javatest1;
+
+public class Code11 {
+	
+	static void a() {
+		int i = 0;
+	}
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i<5; i++) {
+			a();
+			System.out.println(i);
+		}
+		
+	}
+
+}
