39f9752c5be5dda8ef33ff880dba93b22127119d
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-04-05 09:03:15 +0900

    java
    상속

---
+++ javatest1/Code13.java
@@ -0,0 +1,39 @@
+package javatest1;
+
+class Calculator {
+	
+	int left, right;
+	
+	public void setOprands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public void sum() {
+		System.out.println(this.left + this.right);
+	}
+	
+	public void avg() {
+		System.out.println( (this.left + this.right) / 2);
+	}
+
+	
+}
+
+class SubstractionableCalculator extends Calculator {
+	public void substract() {
+		System.out.println(this.left - this.right);
+	}
+}
+
+public class Code13 {
+	
+	public static void main(String [] args) {
+		
+		SubstractionableCalculator c1 = new SubstractionableCalculator();
+		c1.setOprands (10, 20);
+		c1.sum();
+		c1.avg();
+		c1.substract();
+	}
+}
