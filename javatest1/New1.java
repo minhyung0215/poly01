b14564f60ccceba06a629a2a4cff65e9a523b28a
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-04-09 11:42:56 +0900

    java

---
+++ javatest1/New1.java
@@ -0,0 +1,19 @@
+package javatest1;
+
+public class New1 {
+
+	public static void main(String[] args) {
+		int i=2;
+		int j=1;
+		
+		while(i <= 9) {
+			while(j<=9) {
+				System.out.println(i + "X" + j + "=" + (i*j));
+				j++;
+			}
+			j=1;
+			i++;
+		}
+		return;
+	}
+}
