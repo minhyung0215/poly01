54bbfd476942bc729ffb6b5d6176f65fe3bd6127
minhyung0215 <80372021+minhyung0215@users.noreply.github.com>
2021-05-04 08:09:02 +0900

    c

---
+++ 프로그램기초/기본7_9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int hap = 0;
+	int i;
+	for (i = 0; i <= 100; i++) {
+		hap += i;
+		if (hap >= 1000) {
+			break;
+		}
+
+	}
+	printf("1~100이 합에서 최초로 1000이 넘는 위치는? : %d\n", i);
+}
